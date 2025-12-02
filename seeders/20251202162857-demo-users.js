9093dd1cff7244d308f4a08781f4857409eada17
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-12-02 23:35:04 +0700

    feat: add users seeder

---
+++ seeders/20251202162857-demo-users.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const extistingUsers = await queryInterface.sequelize.query(
+      `SELECT id FROM "Users";`,
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    );
+
+    if (extistingUsers.length > 0) {
+      console.log('Users already exist, skipping seeding demo users.');
+      return;
+    }
+    
+    const hashedPassword = bcrypt.hashSync('admin123', 10);
+
+    await queryInterface.bulkInsert('People', [
+      {
+        name: 'Admin',
+        email: 'admin@gmail.com',
+        password: hashedPassword,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);  
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('People', null, {});
+  }
+};
