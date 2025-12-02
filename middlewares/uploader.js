7a19a9129b40782c4d20f8c843b87d5ede91fbfb
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-12-03 00:00:31 +0700

    feat: add uploader middleware

---
+++ middlewares/uploader.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith("image/")){
+            cb(null, true)
+        } else {
+            cb(new Error("Only image files are allowed!"), false)
+        }
+    }
+});
+
+module.exports = { uploader: upload }
